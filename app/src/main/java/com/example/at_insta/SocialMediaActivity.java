1ae13d273b134370f3645299b0f008ed43b8f442
aakashtwinkle <46861807+aakashtwinkle@users.noreply.github.com>
2019-03-08 18:18:25 +0530

    Tab Layout

--- app/src/main/java/com/example/at_insta/SocialMediaActivity.java
+++ app/src/main/java/com/example/at_insta/SocialMediaActivity.java
@@ -9,5 +9,6 @@ public class SocialMediaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        setTitle("Social Media");
     }
 }
